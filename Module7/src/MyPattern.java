ef979edb4512e9aae3bc06c015f2ab0cdb049aee
serg-fam <sergifam@gmail.com>
2016-06-21 16:18:24 +0300

    Comming soon

---
+++ Module7/src/MyPattern.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Serg-fam on 21.06.2016 on 16:15.
+ */
+public class MyPattern {
+
+    public static void main(String[] args) {
+        System.out.println("Wait");
+        System.out.println("Comming soon");
+    }
+}
---
+++ Module8/src/MyCollection.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Serg-fam on 21.06.2016 on 16:16.
+ */
+public class MyCollection {
+    public static void main(String[] args) {
+        System.out.println("Comming soon");
+    }
+}
